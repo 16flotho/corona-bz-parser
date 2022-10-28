5370a53f32d978cba2dd65b635b68535431d583c
16flotho <thomas.florian@tfobz.net>
2022-10-29 00:02:29 +0200

    fix: none in file

--- corona-bz-parser.js
+++ corona-bz-parser.js
@@ -6,7 +6,7 @@ const chartKh = "https://afbs.provinz.bz.it/upload/coronavirus/chart_DE_Kh30.js"
 const chartQuar = "https://afbs.provinz.bz.it/upload/coronavirus/chart_DE_Quar.js";
 
 
-let fieldnameMap = new Map([
+const fieldnameMap = new Map([
     ["chardttPOS", "positive"],
     ["chardttACT", "curPositive"],
     ["chardttHEA", "healed"],
@@ -20,7 +20,7 @@ let fieldnameMap = new Map([
     ["Personen in Quarantäne/häuslicher Isolation", "quarantine"]
 ]);
 
-let date = new Date().toLocaleDateString('de-DE', { day: "2-digit", month: "2-digit", year: "numeric", timeZone: "Europe/Rome" });
+const date = new Date().toLocaleDateString('de-DE', { day: "2-digit", month: "2-digit", year: "numeric", timeZone: "Europe/Rome" });
 
 
 Promise.all([
@@ -33,23 +33,23 @@ Promise.all([
     fs.mkdirSync("data", { recursive: true });
     fs.writeFileSync(`data/${date}.json`, JSON.stringify(data, null, 4));
 }).catch(reason => {
-    console.log(reason)
-    exit(1);
+    console.error(reason);
+    return 1;
 })
 
 async function parseTopBox(res) {
     return new Promise(async (resolve, reject) => {
         let coronaData = {};
-        let text = await res.text();
+        const text = await res.text();
         let lines = text.split("\n");
         lines = lines.filter(entry => entry.includes("span") && !entry.includes("dateNow"));
         for(let i = 0; i < lines.length; i += 2) {
-            let line = lines[i];
+            const line = lines[i];
             if(lines[i + 1]?.includes(date)) {
-                let parsedFieldname = fieldnameMap.get(line.substring(line.indexOf('chardtt'), line.lastIndexOf("'")))
-                let data = line.substring(line.indexOf("<span>") + "<span>".length, line.indexOf('</span>')).split(" ")
-                let value = data[0];
-                let delta = data[1].replaceAll(/\(|\)/g, "");
+                const parsedFieldname = fieldnameMap.get(line.substring(line.indexOf('chardtt'), line.lastIndexOf("'")))
+                const data = line.substring(line.indexOf("<span>") + "<span>".length, line.indexOf('</span>')).split(" ")
+                const value = data[0] ;
+                const delta = data[1].replaceAll(/\(|\)/g, "");
                 if(parsedFieldname != "posRat") {
                     coronaData[parsedFieldname] = parseInt(value);
                     coronaData[parsedFieldname + "_delta"] = parseInt(delta);
@@ -69,16 +69,16 @@ async function parseTopBox(res) {
 async function parseChartData(res) {
     return new Promise(async (resolve, reject) => {
         let coronaData = {};
-        let text = await res.text();
+        const text = await res.text();
         let chartData;
         eval(`chartData = new Object(` + (text.substring(text.indexOf("{"), text.lastIndexOf("}") + 1)) + ")");
-        let columns = chartData.data.columns;
-        let dataRows = columns.length / 2;
+        const columns = chartData.data.columns;
+        const dataRows = columns.length / 2;
         for(let i = 0; i < dataRows; i++) {
-            let lastColumnElementIndex = columns[i].length - 1;
+            const lastColumnElementIndex = columns[i].length - 1;
             if(date.includes(columns[i][lastColumnElementIndex])) {
-                let fieldname = fieldnameMap.get(columns[i + dataRows][0]);
-                coronaData[fieldname] = columns[i + dataRows][lastColumnElementIndex];
+                const fieldname = fieldnameMap.get(columns[i + dataRows][0]);
+                coronaData[fieldname] = columns[i + dataRows][lastColumnElementIndex] ?? 0;
                 coronaData[fieldname + "_delta"] = coronaData[fieldname] - columns[i + dataRows][lastColumnElementIndex - 1]
             } else {
                 reject("Data is not up to date: " + columns[i][lastColumnElementIndex])
